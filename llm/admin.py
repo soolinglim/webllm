fee8ddb1266c074e4e5383d192a66d74a6ed7072
Soo Ling Lim <lim.sooling@gmail.com>
2024-01-03 17:11:02 +0000

    add user input in admin

--- llm/admin.py
+++ llm/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class UserInputAdmin(admin.ModelAdmin):
+    # fields = ('access_key', 'name')
+    list_display = ('pk', 'user_input', 'hide',)
+    ordering = ('-timestamp',)
+
+admin.site.register(UserInput, UserInputAdmin)
